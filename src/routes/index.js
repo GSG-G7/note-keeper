0cfaab6d4208db5c40065dfae1340867b06fa7e1
fares98 <faresalhello21@gmail.com>
2019-08-06 16:03:02 +0300

    Get Request Relate #9

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,18 @@
 const router = require('express').Router();
+const fetch = require('node-fetch');
+const { getData } = require('../helpers');
 
-router.get('/',(req,res)=>{
-  res.render('home')
-})
+router.get('/', (req, res) => {
+  res.render('home', {});
+});
+
+router.get('/paste/:id', (req, res, next) => {
+  getData()
+    .then(data => data.arr[req.params.id - 1].info.link)
+    .then(link => fetch(link)
+      .then(resp => resp.text())
+      .then(data => res.render('paste', { data })))
+    .catch(next);
+});
 
 module.exports = router;
